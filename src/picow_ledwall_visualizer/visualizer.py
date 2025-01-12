533b1dc1262f0185cd3ac2f89f3ade9d45ec0eaf
Giorgio Caizzi <giocaizzi@gmail.com>
2025-01-12 16:38:07 +0100

    visualizer

--- src/picow_ledwall_visualizer/visualizer.py
+++ src/picow_ledwall_visualizer/visualizer.py
@@ -11,13 +11,26 @@ class LedWallVisualizer:
         Args:
             nrows (int): Number of rows.
             ncols (int): Number of columns.
+            grid (np.ndarray): Grid of pixels.
+            objects (List[Text]): List of Text objects.
         """
         self.nrows = nrows
         self.ncols = ncols
         self.grid = np.zeros((nrows, ncols, 3), dtype=int)
+        self.objects = []
 
-    def fill_grid(self, objects=[]):
-        for obj in objects:
+    def add_object(self, obj):
+        """Add a Text object to the LED wall."""
+        self.objects.append(obj)
+
+    def clear(self):
+        """Clear all Text objects from the LED wall."""
+        self.objects = []
+        self._clear_grid()
+
+    def _render(self):
+        """Render the Text objects on the grid (led wall)."""
+        for obj in self.objects:
             # text
             for char in obj.chars:
                 for pixel in char.pixels:
@@ -28,11 +41,12 @@ class LedWallVisualizer:
     def show(self, autoclear=True):
         """Display the LED wall."""
         # create Array-like object from x, y, color
+        self._render()
         plt.imshow(self.grid)
         plt.show()
         if autoclear:
-            self.clear()
+            self._clear_grid()
 
-    def clear(self):
+    def _clear_grid(self):
         """Clear the LED wall."""
         self.grid.fill(0)
